8704ea88ca5db40879b310f39908c4d5ab35febc
SatyabratKumarSingh <satyabrat.kumar.singh@hotmail.com>
2017-02-19 23:08:22 +0000

    changing listbuffer to list.

--- src/main/scala/actors/OrderOperationActor.scala
+++ src/main/scala/actors/OrderOperationActor.scala
@@ -3,24 +3,24 @@ package actors
 import akka.actor.Actor
 import stockorders.{IncomingOrder, Order, OrderOperation}
 
-import scala.collection.mutable.ListBuffer
-
 /**
   * Created by Satya on 16/02/2017.
   */
-class OrderOperationActor(orders: ListBuffer[Order]) extends Actor{
-   override def receive = {
+class OrderOperationActor extends Actor {
+  var orders = List.empty[Order]
+
+  override def receive = {
     case incomingOrder: IncomingOrder => {
-       if(incomingOrder.orderOperation == OrderOperation.Register){
-          println("Registering a new order in the list ")
-          orders += incomingOrder.order
-          sender ! orders
-       }
-      else{
-         println("Un-registering a new order in the list ")
-         orders -= incomingOrder.order
-         sender ! orders
-       }
+      if (incomingOrder.orderOperation == OrderOperation.Register) {
+        println("Registering a new order in the list ")
+        orders = orders :+ incomingOrder.order
+        sender ! orders
+      }
+      else {
+        println("Un-registering a new order in the list ")
+        orders =  orders.filter(x=>x.userId != incomingOrder.order.userId)
+        sender ! orders
+      }
     }
   }
 }
--- src/main/scala/app/ActorApp.scala
+++ src/main/scala/app/ActorApp.scala
@@ -17,29 +17,26 @@ object ActorApp extends App{
 
     val system = ActorSystem("HelloSystem")
     // default Actor constructor
-    val orderActor = system.actorOf(Props(new OrderOperationActor(orderList)), name = "orderActor")
+    val orderActor = system.actorOf(Props(new OrderOperationActor()), name = "orderActor")
 
     val incomingOrder = new IncomingOrder(new Order("SATYA",12,300.20,OrderType.Buy),OrderOperation.Register)
     orderActor ! incomingOrder
-    val incomingOrder1 = new IncomingOrder(new Order("MUNISH",15,400.20,OrderType.Buy),OrderOperation.Register)
+
+        val incomingOrder1 = new IncomingOrder(new Order("MUNISH",15,400.20,OrderType.Buy),OrderOperation.Register)
     orderActor ! incomingOrder1
     val incomingOrder2 = new IncomingOrder(new Order("SAMEER",18,500.20,OrderType.Buy),OrderOperation.Register)
     orderActor ! incomingOrder2
 
-   /* implicit val timeout = Timeout(5 seconds)
+   implicit val timeout = Timeout(5 seconds)
     val future = orderActor ? incomingOrder
-    val result = Await.result(future, timeout.duration).asInstanceOf[ListBuffer[Order]]
-    println(result)*/
-
-    println(orderList)
-    //println(orderList)
-    //println(orderList)
+    val result = Await.result(future, timeout.duration).asInstanceOf[List[Order]]
+    println(result)
 
     val incomingOrder3 = new IncomingOrder(new Order("SAMEER",18,500.20,OrderType.Buy),OrderOperation.UnRegister)
-    orderActor ! incomingOrder3
+    //orderActor ! incomingOrder3
 
-    //val future1 = orderActor ? incomingOrder3
-    //val result1 = Await.result(future, timeout.duration).asInstanceOf[ListBuffer[Order]]
-    println(orderList)
+    val future1 = orderActor ? incomingOrder3
+    val result1 = Await.result(future1, timeout.duration).asInstanceOf[List[Order]]
+    println("Unregistered list " + result1)
 
 }
